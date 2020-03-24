dae2fada3554966237a4c52911012e205617af8f
Solomon Ekrebe <krebedev@gmail.com>
2020-03-24 20:22:20 +0100

    Updates

---
+++ precache-manifest.4d8f3882fd1239fcbb767fe97aa36d45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8af764094ae87504cf1a700ee4fa3e",
+    "url": "/bookfindr/index.html"
+  },
+  {
+    "revision": "4fc75de06613ba5a3d31",
+    "url": "/bookfindr/static/css/main.f1886d8a.chunk.css"
+  },
+  {
+    "revision": "77804c1c4c308352692c",
+    "url": "/bookfindr/static/js/2.716c05ba.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/bookfindr/static/js/2.716c05ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc75de06613ba5a3d31",
+    "url": "/bookfindr/static/js/main.6d492470.chunk.js"
+  },
+  {
+    "revision": "a7fa3437ed9de34e3b29",
+    "url": "/bookfindr/static/js/runtime-main.cd8240b2.js"
+  },
+  {
+    "revision": "b98efed8e6d132702502ade196ce6991",
+    "url": "/bookfindr/static/media/default-cover.b98efed8.png"
+  }
+]);
